5f249db5353366de3af2ca6986f24356a7783759
Geoffrey Fardeau <fardeau.geoffrey@gmail.com>
2023-11-13 18:53:38 +0100

    adding spicyness note

--- ng-hotsauce-app/src/app/sauce/list-sauces/list-sauces.component.ts
+++ ng-hotsauce-app/src/app/sauce/list-sauces/list-sauces.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import  {Sauces} from '../sauces'
 import { Router } from '@angular/router';
 import { SauceService } from '../sauce.service';
@@ -11,17 +11,33 @@ import { SauceService } from '../sauce.service';
 })
 export class ListSaucesComponent implements OnInit {
 saucesList: Sauces[] | undefined
+finalNoteArray: number[] = [];
 
   constructor(private sauceService: SauceService,
               private router: Router) {}
 ngOnInit(){
- this.sauceService.getAllSauces().subscribe(sauceList => this.saucesList = sauceList)
+ this.sauceService.getAllSauces().subscribe(sauceList => {this.saucesList = sauceList
+  this.boucleSauce(sauceList)})
  console.log(this.saucesList)
+
  
 }
+boucleSauce(saucesList: Sauces[] | undefined) {
+    if (saucesList) {
+        saucesList.forEach(sauce => {
+            console.log(sauce.spicyness);
 
+            for (let i = 0; i < sauce.spicyness; i++) {
+                this.finalNoteArray.push(i + 1);
+                console.log(this.finalNoteArray);
+            }
+        });
+    }
+}
 
 goToSauce(sauce: Sauces){
  this.router.navigate(['/sauces', sauce.id])
-}}
+}
+}
+
 
--- ng-hotsauce-app/src/app/sauce/sauce-infos/sauce-infos.component.ts
+++ ng-hotsauce-app/src/app/sauce/sauce-infos/sauce-infos.component.ts
@@ -13,19 +13,32 @@ export class SauceInfosComponent implements OnInit{
   sauceList: Sauces[] | undefined
   sauce: Sauces | undefined
 
-  constructor(private sauceService: SauceService, private route: ActivatedRoute, private router: Router) { }
+  finalNoteArray: number[] = [];
+
+  constructor(private sauceService: SauceService, private route: ActivatedRoute, private router: Router) { 
+  
+  }
   ngOnInit(): void  {
     const sauceId: string | null = this.route.snapshot.paramMap.get('id');
     console.log(sauceId)
 
     if(sauceId) {
-      this.sauceService.getSauceById(+sauceId).subscribe(sauce => this.sauce = sauce)
-
+      let sauce = this.sauceService.getSauceById(+sauceId).subscribe(sauce => {this.sauce = sauce
+        this.boucleSauce(sauce)})
     } else {
       this.sauce = undefined
     }
    
+
+   
   }
+
+  boucleSauce(sauce: Sauces) {
+    for (let i = 0; i < sauce.spicyness ; i++) {
+      console.log(sauce.spicyness)
+      this.finalNoteArray.push(i + 1);
+   
+  }}
 
 deleteSauce(id: any){
   this.sauceService.deleteSauce(id).subscribe((response) => this.router.navigate(['/sauces']));
